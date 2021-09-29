559c3d3756c39e96800c5fc5df73929bb6a70832
Johan <jvo@chaosgeordend.nl>
2021-09-29 08:55:18 +0200

    Add animation time slider

--- compass_widget_test.py
+++ compass_widget_test.py
@@ -23,7 +23,8 @@ class Gui:
 
         self.angle_max = pi  # 180 degrees
         self.angle = 0.0  # rotation angle (rad)
-        self.angle_step = pi / 360  # angle increment steps (rad)
+        self.angle_resolution = pi / 360  # angle increment steps (rad)
+        self.angle_step = 3
         self.speed = 10
 
         # constants
@@ -32,6 +33,7 @@ class Gui:
         self.k1_drag = tk.DoubleVar()  # drag
         self.v0 = tk.DoubleVar()  # initial velocity
         self.h0 = tk.DoubleVar()  # initial height
+        self.max_time = tk.DoubleVar()  # max animation time (s)
 
         self.animation_active = tk.BooleanVar()
         self.animation_duration = tk.DoubleVar()
@@ -84,49 +86,58 @@ class Gui:
                       showvalue=tk.TRUE, orient=tk.HORIZONTAL,
                       command=self.spring_constants,
                       variable=self.k)
-        s1.set(100)
         s1.pack(fill=tk.NONE, side=tk.LEFT)
+        s1.set(100)
 
         s2 = tk.Scale(parameters_frame,
-                      from_=1.0, to=50.0,
+                      from_=1.0, to=10.0, digits=4, resolution=0.0125,
                       label='mass (kg)', length='35m',
                       showvalue=tk.TRUE, orient=tk.HORIZONTAL,
                       command=self.spring_constants,
                       variable=self.m)
-        s2.set(2.0)
         s2.pack(fill=tk.NONE, side=tk.LEFT)
+        s2.set(1.75)
+
+        s3 = tk.Scale(parameters_frame,
+                      from_=0.0, to=10.0, digits=4, resolution=0.0125,
+                      label='t_max (s)', length='35m',
+                      showvalue=tk.TRUE, orient=tk.HORIZONTAL,
+                      command=self.spring_constants,
+                      variable=self.max_time)
+        s3.pack(fill=tk.NONE, side=tk.LEFT)
+        s3.set(1.75)
 
         parameters_frame2 = tk.Frame(root, width=self.width,
                                      padx='2m', pady='2m')
         parameters_frame2.pack(fill=tk.NONE, side=tk.BOTTOM)
 
         # https://stackoverflow.com/questions/25361926/tkinter-scale-and-floats-when-resolution-1
-        s3 = tk.Scale(parameters_frame2,
-                      from_=0.0, to=100.0, digits=4, resolution=0.0125,
+        s4 = tk.Scale(parameters_frame2,
+                      from_=0.0, to=10.0, digits=4, resolution=0.0125,
                       label='k1_drag', length='35m',
                       showvalue=tk.TRUE, orient=tk.HORIZONTAL,
                       command=self.spring_constants,
                       variable=self.k1_drag)
-        s3.set(10.0)
-        s3.pack(fill=tk.NONE, side=tk.LEFT)
+        s4.pack(fill=tk.NONE, side=tk.LEFT)
+        s4.set(5.0)
 
-        s4 = tk.Scale(parameters_frame2,
-                      from_=0.0, to=100.0, digits=4, resolution=0.0125,
+        s5 = tk.Scale(parameters_frame2,
+                      from_=0.0, to=10.0, digits=4, resolution=0.0125,
                       label='v0', length='35m',
                       showvalue=tk.TRUE, orient=tk.HORIZONTAL,
                       command=self.spring_constants,
                       variable=self.v0)
-        s4.set(1.0)
-        s4.pack(fill=tk.NONE, side=tk.LEFT)
+        s5.pack(fill=tk.NONE, side=tk.LEFT)
+        s5.set(1.5)
 
-        s5 = tk.Scale(parameters_frame2,
-                      from_=0.0, to=100.0, digits=4, resolution=0.0125,
-                      label='h0', length='35m',
+        s6 = tk.Scale(parameters_frame2,
+                      from_=-10.0, to=10.0, digits=4, resolution=0.05,
+                      label='h0 (cm)', length='35m',
                       showvalue=tk.TRUE, orient=tk.HORIZONTAL,
                       command=self.spring_constants,
                       variable=self.h0)
-        s5.set(0.0)
-        s5.pack(fill=tk.NONE, side=tk.LEFT)
+        s6.pack(fill=tk.NONE, side=tk.LEFT)
+        s6.set(0.0)
 
         root.bind('<Control-q>', self.quit_click)
         root.protocol("WM_DELETE_WINDOW", self.quit_click)
@@ -160,16 +171,17 @@ class Gui:
         self.compass.k = self.k.get()
         self.compass.m = self.m.get()
         self.compass.k1_drag = self.k1_drag.get()
-        self.compass.h0 = self.h0.get()
+        self.compass.h0 = self.h0.get() / 100
         self.compass.v0 = self.v0.get()
+        self.compass.animation_max_time = self.max_time.get()
 
     def rotate_left(self, event):
-        self.angle += 6 * self.speed * self.angle_step
+        self.angle += self.angle_step * self.speed * self.angle_resolution
         logging.info("self.angle: {}".format(self.angle))
         self.compass.angle = self.angle
 
     def rotate_right(self, event):
-        self.angle -= 6 * self.speed * self.angle_step
+        self.angle -= self.angle_step * self.speed * self.angle_resolution
         logging.info("self.angle: {}".format(self.angle))
         self.compass.angle = self.angle
 
@@ -187,7 +199,7 @@ def run():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    # logging.basicConfig(level=logging.WARNING)
     # logging.basicConfig(level=logging.DEBUG)
+    # logging.basicConfig(level=logging.WARNING)
+    logging.basicConfig(level=logging.INFO)
     run()
